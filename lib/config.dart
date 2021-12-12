4dfec2614613f5aaa6dfb19f354ee9f3fa7cf14c
Abdalqader27 <abdalqader27.najjar@gmail.com>
2021-12-12 14:42:02 +0200

    .

--- lib/config.dart
+++ lib/config.dart
@@ -6,8 +6,7 @@ abstract class ConfigReader {
   static late Map<String, dynamic> _config;
 
   static Future<void> initialize() async {
-    String? configString =
-        await rootBundle.loadString('config/app_config.json');
+    final configString = await rootBundle.loadString('config/app_config.json');
     _config = json.decode(configString) as Map<String, dynamic>;
   }
 
